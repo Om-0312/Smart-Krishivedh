a0655e47f31dbe2b18b7778e792e08979a70c222
Om Dhengle <omdhengle162@gmail.com>
2026-01-31 12:40:53 +0530

    Add files via upload
    Project added!!

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// Top-level build file
+plugins {
+    // RESTORED: Latest AGP 8.12.3
+    id("com.android.application") version "8.12.3" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
